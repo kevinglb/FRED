41e569ce2ec9990c189cb2160c97541e4decf114
Kevin <kevinglbx@gmail.com>
2016-05-24 10:54:16 +0800

    1.fix callback erroe 2.add changeCable & changeShackle afterslideEnd

--- js/FRED-wrapped.js
+++ js/FRED-wrapped.js
@@ -1,8 +1,7 @@
 (function(){
     function FRED(){
         this.init();
-        this.preloadImg(this.initCanvas)
-        
+        this.preloadImg(this.initCanvas);
     }
     FRED.prototype={
         init: function(){
@@ -20,9 +19,11 @@
             _this.cableSwiper = null;
             _this.shackleSwiperIndex = 0;
             _this.imgArr = [];
+            _this.selectedCable = null;
+            _this.selectedShackle = null;
 
             _this.startFRED();
-            _this.initSwiperSize(_this.initSwiper);
+            _this.initSwiperSize();
         },
 
         preloadImg: function(callback){
@@ -41,30 +42,25 @@
                             {"name":"orgris","src": "../FRED/img/bg/bg-orgris.jpg"},
                             {"name":"bleu","src": "../FRED/img/bg/bg-bleu.jpg"}];
             for(var i=0;i<_this.imgArr.length;i++){
-                var img = new Image();
-                img.src = _this.imgArr[i].src;
-                img.onload = function(){
-                    _this.imgArr['img'] = img;
+                var image = new Image();
+                image.src = _this.imgArr[i].src;
+                _this.imgArr[i].image= image;
+                image.onload = function(){
                     num++;
-                };
-                if(num >= _this.imgArr.length){
-                    _this.initCanvas();
+                    if(num >= _this.imgArr.length && typeof callback == "function"){
+                        //console.log(callback);
+                        callback(); //the default 'this' in callback will refer to window scoop
+                    };
                 }
             }
             //or using jQuery to insert css into head tag
-            $('head').append('<link rel="stylesheet" href="css/image.css" type="text/css" />');
-            
-            
-            // if(typeof callback == "function"){
-            //     console.log(callback);
-            //     callback();
-            // }   
+            $('head').append('<link rel="stylesheet" href="css/image.css" type="text/css" />'); 
         },
 
         startFRED: function(){
             var _this = this;
             $('#beginPageStart,#goToPlayStart').click(function(){
-                $('.beginFred').fadeOut(600,function(){
+                $('.beginFred').fadeOut(500,function(){
                     $('.readyPageContent').fadeIn(1000,function(){
                         $('.readyPageSkip').fadeIn(1000,function(){
                             $('.swiper-pagination').fadeIn(200)
@@ -73,21 +69,22 @@
                 });
             });
             $('#fredRules').click(function(){
-                $('.beginContainer').fadeOut(600,function(){
-                    $('.detailsPageContainer').fadeIn(600);
+                $('.beginContainer').fadeOut(500,function(){
+                    $('.detailsPageContainer').fadeIn(500);
                 });
             });
             $('#nextPage').click(function(){
-                $('.detailsPageContainer').fadeOut(600,function(){
-                    $('.goToPlayContainer').fadeIn(600);
+                $('.detailsPageContainer').fadeOut(500,function(){
+                    $('.goToPlayContainer').fadeIn(500);
                 });
             });
             $('#readyPageSkip,#closeBtn').click(function(){
                 $('.readyPage').fadeOut();
             });
         },
+
         initCanvas: function(){
-            var _this = this;
+            var _this = this === window ? window.FRED : this;
             _this.canvas.width = _this.innerWidth;
             _this.canvas.height = _this.innerHeight;
             _this.drawCanvas();
@@ -95,13 +92,13 @@
 
         updateCanvas: function(){//del(context)
             var _this = this;
-            //prevOrNext();
+            _this.detectDir();
             var a=0;
             var b=setInterval(function(){
                 _this.context.save();//保存状态
                 a+=10;//擦除速度
                 _this.context.rotate(45*Math.PI/180);//canvas旋转角度
-                _this.context.clearRect(0,-600,a,1100);//擦除方块，需要调整
+                _this.context.clearRect(0,-500,a,1100);//擦除方块，需要调整
                 _this.context.restore();//回归状态
                 _this.context.stroke();//执行
                 if(a >= 750){
@@ -113,49 +110,66 @@
 
         drawCanvas: function(index){
             var _this = this;
-            var i =0;
-            var canvasBg = _this.imgArr[i]['img'];//还需要创建初始图片数组
-            //console.log(canvasBg);  
-            // // canvasBg.onload=function(){//需要后期优化
+            var i =_this.shackleSwiperIndex;
+            var canvasBg = _this.imgArr[i].image;//还需要创建初始图片数组
             _this.context.drawImage(canvasBg,0,0,_this.canvas.width,_this.canvas.height);//在canvas上画背景
         },
 
-
-        initSwiperSize: function(callback){
+        initSwiperSize: function(){
             var _this = this;
             var rotateAngle = 37*Math.PI/180.0;
             var areaHeight = Math.floor((_this.innerHeight*0.5)*Math.cos(rotateAngle)),
                 areaWidth = Math.floor(_this.innerHeight*0.5*Math.sin(rotateAngle)+_this.innerWidth*Math.cos(rotateAngle)),
                 midWidth = Math.ceil(_this.innerWidth/Math.cos(37*Math.PI/180.0) + _this.innerWidth*Math.tan(rotateAngle)*Math.cos(53*Math.PI/180.0)),
                 midHeight = Math.ceil(_this.innerWidth*Math.sin(rotateAngle))+1;
+
             _this.$topArea.css({'height':areaHeight,'width':areaWidth,'left':_this.innerWidth - areaWidth});
             _this.$botArea.css({'height':areaHeight,'width':areaWidth});
             _this.$midArea.css({'height':midHeight, 'width':midWidth,'left':(_this.innerWidth-midWidth)/2,'top':Math.ceil((_this.innerHeight-midHeight)/2)});
             _this.$midBg.css({'height':midHeight+1, 'width':midWidth,'left':(_this.innerWidth-midWidth)/2,'top':Math.ceil((_this.innerHeight-midHeight)/2)});
+
             $(".completeBtn").css("margin-left", -($(".completeBtn").width()/2));
+            $(".completeBtn").bind('click',function(){
+                $(".completeFred").addClass("active");
+                _this.initForm();
+                setTimeout(function(){
+                    $(".wrap").hide();
+                },300);
+        
+            });
 
-            if(typeof callback === "function"){
-                callback(_this); //pass the FRED scoop in case that 'this' in the callback refers to window
-            }else{
-                return;
-            }
-            //_this.initSwiper();
+            _this.initSwiper();
 
+            // if(typeof callback == "function"){
+            //     callback(); //the default 'this' in callback will refer to window scoop
+            // }else{
+            //     return;
+            // }
         },
 
-        initSwiper: function(target){
+        initSwiper: function(){
             //console.log(this == window);
             // var _this = this === window ? this.FRED : this;
-            var _this = target;
+            var _this = this;
+            //console.log(_this);
             
-            _this.shackleSwiper1 = new Swiper('.shackle-container1',{shortSwipes : true,onSlideChangeStart: function(swiper){debugger;console.log(_this);_this.updateCanvas()},touchAngle:80,resistance:false,resistanceRatio:0.9,touchRatio : 0.8,watchSlidesProgress : true}),
+            _this.shackleSwiper1 = new Swiper('.shackle-container1',{shortSwipes : true,onSlideChangeStart: function(swiper){_this.updateCanvas()},onSlideChangeEnd: function(swiper){_this.changeShackle()},touchAngle:80,resistance:false,resistanceRatio:0.9,touchRatio : 0.8,watchSlidesProgress : true}),
             _this.shackleSwiper2 = new Swiper('.shackle-container2',{shortSwipes : true, touchAngle:80,resistance : false,resistanceRatio:0.9}),
-            _this.cableSwiper = new Swiper('.cable-container',{shortSwipes : true, touchAngle:80,resistance : false,resistanceRatio:0.9});
+            _this.cableSwiper = new Swiper('.cable-container',{shortSwipes : true, touchAngle:80,resistance : false,resistanceRatio:0.9,onSlideChangeEnd: function(swiper){_this.changeCable()}});
             _this.shackleSwiper1.params.control = _this.shackleSwiper2;
             _this.shackleSwiper2.params.control = _this.shackleSwiper1;
             // _this.shackleSwiperIndex = _this.shackleSwiper1.progress;
         },
 
+        changeCable: function(){
+            var _this = this;
+            _this.selectedCable = $(".cable-container").find(".swiper-slide-active").find('.cableImg').attr("data-cable");
+        },
+
+        changeShackle: function(){
+            var _this = this;
+            _this.selectedShackle = $(".shackle-container1").find(".swiper-slide-active").find('.shackleImg').attr("data-cable");
+        },
 
         detectDir: function(){ //detecet the direction of Swiper animation: prev or ntxt
             var _this = this;
@@ -165,17 +179,29 @@
                 console.log('next');
                 $(".bgPrev").hide();
                 $(".bgNext").show();
-                $(".bgNext").attr("data-bg",dataArr[index]);
-                $(".bgPrev").attr("data-bg",dataArr[index-1]);
+                $(".bgNext").attr("data-bg",_this.imgArr[index].name);
+                $(".bgPrev").attr("data-bg",_this.imgArr[Math.abs(index-1)].name);
             }else if(index < _this.shackleSwiperIndex){
                 console.log('prev')
                 $(".bgPrev").show();
                 $(".bgNext").hide();
-                $(".bgNext").attr("data-bg",dataArr[index-1]);
-                $(".bgPrev").attr("data-bg",dataArr[index]);
+                $(".bgNext").attr("data-bg",_this.imgArr[Math.abs(index-1)].name);
+                $(".bgPrev").attr("data-bg",_this.imgArr[index].name);
             }
             _this.shackleSwiperIndex = index;
+        },
+
+        initForm(){
+            var _this = this;
+            $("#formSubmit").bind('click',function(){
+                $(".shareWrap").addClass("active");
+            });
+        },
+
+        completeForm(){
+            var _this = this;
         }
+
 
     };
     window.FRED = new FRED();
